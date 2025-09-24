9a02e06f1ca735453d3dce8ff3ff882c3565eeb3
Finley Bourke <147405702+bourkeybeans@users.noreply.github.com>
2025-09-24 19:22:47 +0100

    Update db.py

--- db.py
+++ db.py
@@ -1,7 +1,7 @@
 from supabase import create_client
 import os
 
-url = os.getenv("https://oqfyxbkyupiafnvnixqf.supabase.co")
-key = os.getenv("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9xZnl4Ymt5dXBpYWZudm5peHFmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg3MjcxMzcsImV4cCI6MjA3NDMwMzEzN30.oDUvy-_tqPgrtfYuEHb4sRIRpoxr4nyefYSVHKWJvzE")
+url = os.getenv("YOURURL")
+key = os.getenv("YOURKEY")
 
 supabase = create_client(url, key)
